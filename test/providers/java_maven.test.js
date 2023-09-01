cbd9156834ab870e9ad1f577696fc989e5354d82
Ruben Romero Montes <rromerom@redhat.com>
2023-09-01 18:29:56 +0200

    fix: missing common transitive dependencies
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- test/providers/java_maven.test.js
+++ test/providers/java_maven.test.js
@@ -25,7 +25,8 @@ suite('testing the java-maven data provider', () => {
 		"pom_deps_with_ignore_on_wrong",
 		"pom_deps_with_no_ignore",
 		"poms_deps_with_ignore_long",
-		"poms_deps_with_no_ignore_long"
+		"poms_deps_with_no_ignore_long",
+		"poms_deps_with_no_ignore_common_paths"
 	].forEach(testCase => {
 		let scenario = testCase.replace('pom_deps_', '').replaceAll('_', ' ')
 		// test(`custom adhoc test`, async () => {
