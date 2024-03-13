6e8a9ada544da383b4e6c6a1322c8cb90dd7db2c
Zvi Grinberg <zgrinber@redhat.com>
2024-03-13 11:40:57 +0200

    test: fix performance issue in python version >= 3.11.X
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- test/providers/python_pip.test.js
+++ test/providers/python_pip.test.js
@@ -8,6 +8,48 @@ import {getCustomPath } from "../../src/tools.js"
 
 
 let clock
+
+async function sharedComponentAnalysisTestFlow(testCase,usePipDepTreeUtility) {
+	// load the expected list for the scenario
+	let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/expected_component_sbom.json`,).toString().trim()
+	expectedSbom = JSON.stringify(JSON.parse(expectedSbom))
+	// read target manifest file
+	let manifestContent = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/requirements.txt`).toString()
+	// invoke sut stack analysis for scenario manifest
+	let opts = { "EXHORT_PIP_USE_DEP_TREE" : usePipDepTreeUtility }
+	let providedDatForComponent = await pythonPip.provideComponent(manifestContent,opts)
+	// verify returned data matches expectation
+	expect(providedDatForComponent).to.deep.equal({
+		ecosystem: 'pip',
+		contentType: 'application/vnd.cyclonedx+json',
+		content: expectedSbom
+	})
+}
+
+async function sharedStackAnalysisTestFlow(testCase,usePipDepTreeUtility) {
+	// load the expected graph for the scenario
+	let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/expected_stack_sbom.json`,).toString()
+	expectedSbom = JSON.stringify(JSON.parse(expectedSbom))
+	// invoke sut stack analysis for scenario manifest
+	let pipPath = getCustomPath("pip3");
+	execSync(`${pipPath} install -r test/providers/tst_manifests/pip/${testCase}/requirements.txt`, err => {
+		if (err) {
+			throw new Error('fail installing requirements.txt manifest in created virtual python environment --> ' + err.message)
+		}
+	})
+	let opts = { "EXHORT_PIP_USE_DEP_TREE" : usePipDepTreeUtility }
+	let providedDataForStack = await pythonPip.provideStack(`test/providers/tst_manifests/pip/${testCase}/requirements.txt`,opts)
+	// new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date
+
+	// providedDataForStack.content = providedDataForStack.content.replaceAll("\"timestamp\":\"[a-zA-Z0-9\\-\\:]+\"","")
+	// verify returned data matches expectation
+	expect(providedDataForStack).to.deep.equal({
+		ecosystem: 'pip',
+		contentType: 'application/vnd.cyclonedx+json',
+		content: expectedSbom
+	})
+}
+
 suite('testing the python-pip data provider', () => {
 	[
 		{name: 'requirements.txt', expected: true},
@@ -24,45 +66,28 @@ suite('testing the python-pip data provider', () => {
 	].forEach(testCase => {
 		let scenario = testCase.replace('pip_requirements_', '').replaceAll('_', ' ')
 		test(`verify requirements.txt sbom provided for stack analysis with scenario ${scenario}`, async () => {
-			// load the expected graph for the scenario
-			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/expected_stack_sbom.json`,).toString()
-			expectedSbom = JSON.stringify(JSON.parse(expectedSbom))
-			// invoke sut stack analysis for scenario manifest
-			let pipPath = getCustomPath("pip3");
-			execSync(`${pipPath} install -r test/providers/tst_manifests/pip/${testCase}/requirements.txt`, err => {
-				if (err) {
-					throw new Error('fail installing requirements.txt manifest in created virtual python environment --> ' + err.message)
-				}
-			})
-			let providedDataForStack = await pythonPip.provideStack(`test/providers/tst_manifests/pip/${testCase}/requirements.txt`)
-			// new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date
-
-			// providedDataForStack.content = providedDataForStack.content.replaceAll("\"timestamp\":\"[a-zA-Z0-9\\-\\:]+\"","")
-			// verify returned data matches expectation
-			expect(providedDataForStack).to.deep.equal({
-				ecosystem: 'pip',
-				contentType: 'application/vnd.cyclonedx+json',
-				content: expectedSbom
-			})
-		// these test cases takes ~2500-2700 ms each pr >10000 in CI (for the first test-case)
+			await sharedStackAnalysisTestFlow(testCase,false);
+			// these test cases takes ~2500-2700 ms each pr >10000 in CI (for the first test-case)
 		}).timeout(process.env.GITHUB_ACTIONS ? 30000 : 10000)
 
 		test(`verify requirements.txt sbom provided for component analysis with scenario ${scenario}`, async () => {
-			// load the expected list for the scenario
-			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/expected_component_sbom.json`,).toString().trim()
-			expectedSbom = JSON.stringify(JSON.parse(expectedSbom))
-			// read target manifest file
-			let manifestContent = fs.readFileSync(`test/providers/tst_manifests/pip/${testCase}/requirements.txt`).toString()
-			// invoke sut stack analysis for scenario manifest
-			let providedDatForComponent = await pythonPip.provideComponent(manifestContent)
-			// verify returned data matches expectation
-			expect(providedDatForComponent).to.deep.equal({
-				ecosystem: 'pip',
-				contentType: 'application/vnd.cyclonedx+json',
-				content: expectedSbom
-			})
+			await sharedComponentAnalysisTestFlow(testCase,false);
 			// these test cases takes ~1400-2000 ms each pr >10000 in CI (for the first test-case)
 		}).timeout(process.env.GITHUB_ACTIONS ? 15000 : 10000)
+
+		test(`verify requirements.txt sbom provided for stack analysis using pipdeptree utility with scenario ${scenario}`, async () => {
+			await sharedStackAnalysisTestFlow(testCase,true);
+			// these test cases takes ~2500-2700 ms each pr >10000 in CI (for the first test-case)
+		}).timeout(process.env.GITHUB_ACTIONS ? 30000 : 10000)
+
+		test(`verify requirements.txt sbom provided for component analysis using pipdeptree utility with scenario ${scenario}`, async () => {
+			await sharedComponentAnalysisTestFlow(testCase,true);
+			// these test cases takes ~1400-2000 ms each pr >10000 in CI (for the first test-case)
+		}).timeout(process.env.GITHUB_ACTIONS ? 15000 : 10000)
+
+
+
+
 	});
 
 }).beforeAll(() => clock = sinon.useFakeTimers(new Date('2023-10-01T00:00:00.000Z'))).afterAll(()=> clock.restore());
