5f0b940cd8a0b25bdbd62321320954ca9e3e6705
Zvi Grinberg <zgrinber@redhat.com>
2024-05-05 17:29:25 +0300

    fix: make tests works in both Node 18 and Latest version ( up to date - Node Version 22)
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import path from "node:path";
 import {EOL} from "os";
 import { availableProviders, match } from './provider.js'
 import {AnalysisReport} from '../generated/backend/AnalysisReport.js'
 import analysis from './analysis.js'
 import fs from 'node:fs'
 import {getCustom} from "./tools.js";
-import PackageJson from '../package.json' with { type: 'json'};
+import.meta.dirname
+import * as url from 'url';
+// const packageJson = await import ('../package.json',{ with: { type: 'json' } })
 
 export default { AnalysisReport, componentAnalysis, stackAnalysis, validateToken }
 
@@ -21,10 +24,10 @@ function logOptionsAndEnvironmentsVariables(alongsideText,valueToBePrinted) {
 
 }
 
-/** This function is used to determine exhort url backend according to the following logic:
+/** This function is used to determine exhort theUrl backend according to the following logic:
  * If EXHORT_DEV_MODE = true, then take the value of the EXHORT BACKEND URL of dev/staging environment in such a way:
  * take it as environment variable if exists, otherwise, take it from opts object if exists, otherwise, use the hardcoded default of DEV environment.
- * If EXHORT_DEV_MODE = false , then select the production url of EXHORT Backend, which is hardcoded.
+ * If EXHORT_DEV_MODE = false , then select the production theUrl of EXHORT Backend, which is hardcoded.
  * EXHORT_DEV_MODE evaluated in the following order and selected when it finds it first:
  * 1. Environment Variable
  * 2. (key,value) from opts object
@@ -34,8 +37,14 @@ function logOptionsAndEnvironmentsVariables(alongsideText,valueToBePrinted) {
  * @private
  */
 function selectExhortBackend(opts= {}) {
-
-	logOptionsAndEnvironmentsVariables("exhort-javascript-api analysis started, version: ",PackageJson.version)
+// new ESM way in nodeJS ( since node version 22 ) to bring module directory.
+	let dirName = import.meta.dirname
+// old ESM way in nodeJS ( before node versions 22.00 to bring module directory.
+	if (!dirName) {
+		dirName = url.fileURLToPath(new URL('.', import.meta.url));
+	}
+	let packageJson = JSON.parse(fs.readFileSync(path.join(dirName, "..","package.json" )).toString())
+	logOptionsAndEnvironmentsVariables("exhort-javascript-api analysis started, version: ",packageJson.version)
 	let result
 	let exhortDevModeBundled = "false"
 	let exhortDevMode = getCustom("EXHORT_DEV_MODE",exhortDevModeBundled,opts)
@@ -63,10 +72,10 @@ export function testSelectExhortBackend(opts)
 }
 
 /**
- * @type {string} backend url to send requests to
+ * @type {string} backend theUrl to send requests to
  * @private
  */
-let url
+let theUrl
 
 /**
  * Get stack analysis report for a manifest file.
@@ -78,10 +87,10 @@ let url
  * 		or backend request failed
  */
 async function stackAnalysis(manifest, html = false, opts = {}) {
-	url = selectExhortBackend(opts)
+	theUrl = selectExhortBackend(opts)
 	fs.accessSync(manifest, fs.constants.R_OK) // throws error if file unreadable
 	let provider = match(manifest, availableProviders) // throws error if no matching provider
-	return await analysis.requestStack(provider, manifest, url, html, opts) // throws error request sending failed
+	return await analysis.requestStack(provider, manifest, theUrl, html, opts) // throws error request sending failed
 }
 
 /**
@@ -93,12 +102,12 @@ async function stackAnalysis(manifest, html = false, opts = {}) {
  * @throws {Error} if no matching provider, failed to get create content, or backend request failed
  */
 async function componentAnalysis(manifestType, data, opts = {}, path = '') {
-	url = selectExhortBackend(opts)
+	theUrl = selectExhortBackend(opts)
 	let provider = match(manifestType, availableProviders) // throws error if no matching provider
-	return await analysis.requestComponent(provider, data, url, opts,path) // throws error request sending failed
+	return await analysis.requestComponent(provider, data, theUrl, opts,path) // throws error request sending failed
 }
 
 async function validateToken(opts = {}) {
-	url = selectExhortBackend(opts)
-	return await analysis.validateToken(url, opts) // throws error request sending failed
+	theUrl = selectExhortBackend(opts)
+	return await analysis.validateToken(theUrl, opts) // throws error request sending failed
 }
